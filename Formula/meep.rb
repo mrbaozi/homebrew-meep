c8346bacbe70c289f5876f5a37a0d1ba4f787f7d
Yannick Augenstein <yannick.augenstein@me.com>
2018-03-04 01:03:05 +0100

    Remove head because autogen doesn't work

--- Formula/meep.rb
+++ Formula/meep.rb
@@ -4,7 +4,6 @@
  class Meep < Formula
   desc "free finite-difference time-domain (FDTD) software for electromagnetic simulations"
   homepage "https://meep.readthedocs.io/en/latest/"
-  head "https://github.com/stevengj/meep.git", :revision => "f33efca4b8f0cd373f641babd9b8250d725c0455"
   url "https://github.com/stevengj/meep/releases/download/v1.4.3/meep-1.4.3.tar.gz"
   sha256 "492456349c59e923919fcefed16bb1fab358d23eeae77c720cdaf13130a83672"
 
@@ -20,24 +19,16 @@
   depends_on "h5utils"
 
   def install
-    if build.head?
-      system "./autogen.sh", "--enable-shared",
-                             "--with-mpi",
-                             "--disable-debug",
-                             "--disable-dependency-tracking",
-                             "--disable-silent-rules",
-                             "PYTHON=python3",
-                             "--prefix=#{prefix}"
-    end
-    if build.stable?
-      system "./configure", "--disable-debug",
-                            "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--enable-shared",
-                            "--with-mpi",
-                            "PYTHON=python3",
-                            "--prefix=#{prefix}"
-    end
+    args = [
+        "--disable-debug",
+        "--disable-dependency-tracking",
+        "--disable-silent-rules",
+        "--enable-shared",
+        "--with-mpi",
+        "PYTHON=python3",
+        "--prefix=#{prefix}"
+    ]
+    system "./configure", *args
     system "make", "install" # if this fails, try separate make/make install steps
   end
 end
